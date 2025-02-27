491443d8d70cf42547c6c16eaf219f95561212b9
CityRideoneIslam <nahian.islam04@gmail.com>
2025-02-27 19:09:14 +0000

    Initial commit

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "Hello world"
+
+@app.route("/login", methods=["GET"])
+def login():
+    if request.method == "GET":
+        pass
+
+@app.route("/register", methods=["POST", "GET"])
+def register():
+    if request.method == "GET":
+        pass
+
+    if request.method == "POST":
+        pass
 
 if __name__ == "__main__":
     app.run(debug=True)
